5c812f39d12b06f6baa9653cf242ac980f57aa1c
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-10-16 13:59:12 +0200

    Refactor and add generalized search

--- src/BreadthFirstSearch.py
+++ src/BreadthFirstSearch.py
@@ -1,7 +1,9 @@
 import lib.vector as Vector
 
-#Parcours en largeur simple à partir d'un sommet donné en paramètre
+
 def largeur(G, i):
+    #Parcours en largeur simple à partir d'un sommet donné en paramètre
+    
     # Initialisation
     Visite = Vector.initVect(len(G), False) #Quels sommets ont déjà été visités
     File = [i] #Liste d'attente
@@ -19,3 +21,31 @@ def largeur(G, i):
                 File.append(succ) #Et on l'ajoute à la liste d'attente
             # sinon révisite
     return ordreVisite #On renvoit la liste finale dans l'ordre de visite
+
+def largeurG(G):
+    
+    # Effectue le parcours en largeur généralisé à tout le graphe G
+ 
+    Visite = Vector.initVect(len(G), 0)
+    ordreVisite = []
+    File = []
+    for i in range(1, len(G)):
+        if Visite[i] == 0:
+            ordreSousVisite = []
+            File.append(i)
+            Visite[i] = 1
+            #Parcours à partir de i
+            while File:
+                y = File.pop(0)
+                ordreSousVisite.append(y)
+                # Visite de y
+                for z in G[y]:
+                    if Visite[z] == 0:
+                        Visite[z] = 1
+                        File.append(z)
+                        #On ajoute z à la file d'attente
+                        else:
+                        #Revisite de z
+                        pass
+            ordreVisite.append(ordreSousVisite)
+    return ordreVisite
