962b5d7addf2ccbccb25d0a50d30c563eef5b755
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-10-16 13:55:11 +0200

    Add lib vect

--- src/BreadthFirstSearch.py
+++ src/BreadthFirstSearch.py
@@ -19,18 +19,3 @@ def largeur(G, i):
                 File.append(succ) #Et on l'ajoute à la liste d'attente
             # sinon révisite
     return ordreVisite #On renvoit la liste finale dans l'ordre de visite
-	
-##JEU DE TESTS
-
-G1 = [[],[5],[1,4],[2],[3],[2,4]]
-G2 = [[],[5],[1,4,5],[2,4],[],[4]]
-G3 = [[],[3,5,6],[1],[2,4],[],[],[4]]
-Petersen = [[],[2,5,6],[1,3,7],[2,4,8],[3,5,9],[1,4,10],[1,8,9],[2,9,10],[3,6,10],[4,6,7],[5,7,8]]
-
-
-  print(largeur(G1, 1)) ## ---> [1, 5, 2, 4, 3]
-	print(largeur(G2, 1)) ## ---> [1, 5, 4]
-	print(largeur(G3, 1)) ## ---> [1, 3, 5, 6, 2, 4]
-	print(largeur(Petersen, 1)) ## ---> [1, 2, 5, 6, 3, 7, 4, 10, 8, 9]
-	print(largeur(G1, 3)) ## ---> [3, 2, 1, 4, 5]
-	print(largeur(Petersen, 7)) ## ---> [7, 2, 9, 10, 1, 3, 4, 6, 5, 8]
