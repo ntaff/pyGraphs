21f6779470342d32243f8c9969a18673a037ab74
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-10-16 15:45:05 +0200

    Update DepthFirstSearch.py

--- src/DepthFirstSearch.py
+++ src/DepthFirstSearch.py
@@ -1,15 +1,16 @@
 import lib.vect as Vector
 
+# Fonction auxiliaire récursive qui provoque un parcours en profondeur du graphe à partir d'un sommet i
 def profRec(G, i, Visite, ordreVisite):
 
-   # Fonction auxiliaire récursive qui provoque un parcours en profondeur du graphe à partir d'un sommet i
-   # Met à jour les paramètres Visite et ordreVisite
-   
-    Visite[i] = 1
     #Début du parcours de i
-    ordreVisite.append(i)  # Première visite
+    Visite[i] = 1
+
+    # Première visite
+    ordreVisite.append(i)  
 
     for y in G[i]:
+        #Si on a pas visité y
         if Visite[y] == 0:
             profRec(G, y, Visite, ordreVisite)
         else:
@@ -18,20 +19,16 @@ def profRec(G, i, Visite, ordreVisite):
 
     # Fin du parcours de i
 
-
+# Effectue le parcours en profondeur du graphe G à partir d'un sommet i
 def profond(G, i):
 
-   # Effectue le parcours en profondeur du graphe G à partir d'un sommet i
-  
     Visite = Vector.initVect(len(G), 0)
     ordreVisite = []
     profRec(G, i, Visite, ordreVisite)
     return ordreVisite
 
-
+# Effectue le parcours en profondeur généralisé à tout le graphe G
 def profondG(G):
-
-   # Effectue le parcours en profondeur généralisé à tout le graphe G
 
     Visite = Vector.initVect(len(G), 0)
     ordreVisite = []
@@ -39,7 +36,7 @@ def profondG(G):
     for i in range(1, len(G)):
         if Visite[i] == 0:
             ordreSousVisite = []
-            profRec(G, i, Visite, ordreSousVisite)
-            ordreVisite.append(ordreSousVisite)
+            profRec(G, i, Visite, ordreVisiteRec)
+            ordreVisite.append(ordreVisiteRec)
 
     return ordreVisite
